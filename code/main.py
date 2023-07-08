40ede049c31c2550a0c6e23cfa8878ca5bb2976a
Miguel Á. Cantarero <miguel.cantarero.lopez@gmail.com>
2023-07-08 14:07:27 +0200

    1x3rod version compiles and on testing stage
    second version (1x3 rod)  compiles, tested only for dummyexample.txt file

--- code/main.py
+++ code/main.py
@@ -1,9 +1,6 @@
-#https://github.com/MAN1986/pyamaze/blob/main/Demos/A-Star/aStar.py#L50
-#https://www.w3schools.com/python/python_lists_methods.asp
-
 from queue import PriorityQueue
 import sys
- 
+from rod import Rod
 
 
 def readLabyrinth(filename):
@@ -27,7 +24,6 @@ def readLabyrinth(filename):
 def reconstructPath(cameFrom, current):
     """This function iterates a path of cells using a dictionary with the format cell->child"""
     total_path=[current]
-    print("camefrom", cameFrom)
     while current in cameFrom.keys():
         current = cameFrom[current]
         total_path.append(current)
@@ -40,178 +36,140 @@ def reconstructPath(cameFrom, current):
 
 
 def computeRod(cell,orientation,map):
+    """Computes a rod based on his middle cell and the orientation 0-> horizontal, 1-> vertical. Returns a rod object if 
+    it is possible to create a 1x3 rod with these parameters"""
     numRows=len(map)
     numCols=len(map[0])
 
     if orientation==0:
-       if cell[1]-1>=0 and cell[1]+1<numCols:
+       if cell[1]-1>=0 and map[cell[0]][cell[1]-1]!='#' and cell[1]+1<numCols and map[cell[0]][cell[1]+1]!='#':
             left=(cell[0],cell[1]-1)
             right=(cell[0],cell[1]+1)
-            return [left,cell,right]
+            return Rod(0,[left,cell,right])
 
     if orientation==1:
-       if cell[0]-1>=0 and cell[0]+1<numRows:
+       if cell[0]-1>=0 and map[cell[0]-1][cell[1]]!='#' and cell[0]+1<numRows and map[cell[0]+1][cell[1]]!='#':
             top=(cell[0]-1,cell[1])
             bottom=(cell[0]+1,cell[1])
-            return [top,cell,bottom]
+            return Rod(1,[top,cell,bottom])
     
-    return[]
+    return -1
 
-
-
-###not used care.
-def d(cell1,cell2):
-    """This function computes the Mahattan distance,is a distance measure that is calculated by taking the sum of distances between the x and y coordinates."""
-    x1,y1=cell1
-    x2,y2=cell2
-
-    return abs(x1-x2) + abs(y1-y2)
-
-
-def h(rod1,rod2):
-    """This function computes the Mahattan distance,is a distance measure that is calculated by taking the sum of distances between the x and y coordinates."""
-    #x2,y2=cell2
-
-    #rod1=computeRod(cell1)
-    #rod2=computeRod(cell2)
-   
-    distance=0
-
-    for i in range(len(rod1)):
-        distance+=abs(rod1[i][1]-rod2[i][1]) + abs(rod1[i][2]-rod2[i][2])
-
-    return distance
-
-
-def computeNeighbors(map,cell,orientation):
-    """Computes all the possible neighbors given a static cell"""
-    numRows=len(map)
-    numCols=len(map[0])
-
-    rod=computeRod(cell,orientation,map)
-    neighbors=[]
-
-    for m in 'RNSWE':
-        newRod=[]
-        if m=='R':
-          if orientation==0:
-            if rod[1][1]+1<numCols and rod[1][0]-1<numRows and map[rod[1][0]-1,rod[1][1]+1]!='#':
-                if rod[2][1]-1<numCols and rod[2][0]+1<numRows and map[rod[2][0]+1,rod[2][1]-1]!='#':
-                   neighbors().append([(rod[1][0]-1,rod[1][1]+1),(cell),(rod[2][0]+1, rod[2][1]-1)])               
-          if orientation==1:
-             if rod[1][1]-1<numCols and rod[1][0]+1<numRows and map[rod[1][0]+1,rod[1][1]-1]!='#':
-                if rod[2][1]+1<numCols and rod[2][0]-1<numRows and map[rod[2][0]-1,rod[2][1]+1]!='#':
-                   neighbors().append([(rod[1][0]+1,rod[1][1]-1),(cell),(rod[2][0]-1,rod[2][1]+1)])
-      
-        if m=='N':
-          valid=True
-          for c in rod:
-             newRod.append(c)
-             if not c[0]-1>=0 and map[c[0]-1][c[1]]!='#':
-                valid=False
-          if valid:
-            neighbors().append(newRod)
-
-        if m=='S':
-          valid=True
-          for c in rod:
-             newRod.append(c)
-             if not c[0]+1<numRows and map[c[0]+1][c[1]]!='#':
-                valid=False
-          if valid:
-            neighbors().append(newRod)
-
-        if m=='W':
-          valid=True
-          for c in rod:
-             newRod.append(c)
-             if not c[1]-1>=0 and map[c[0]][c[1]-1]!='#':
-                valid=False
-          if valid:
-            neighbors().append(newRod)
-
-        if m=='E':
-          valid=True
-          for c in rod:
-             newRod.append(c)
-             if not c[1]+1<numCols and map[c[0]][c[1]+1]!='#':
-                valid=False
-          if valid:
-            neighbors().append(newRod)
-
-    return neighbors
 
 
 #Based on pseudocode from https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode
 def aStar(map, start, goal):
-    """Performs A* algorithm and computes the minimal path from starting cell to goal cell"""
+    """Performs A* algorithm and computes the minimal path from starting rod to goal rod"""
     numRows=len(map)
     numCols=len(map[0])
+    # The set of discovered nodes that may need to be (re-)expanded.
+    # Initially, only the start node is known.
+    # We use a PriorityQueue to have O(log(N)) efficiency while sorting
     openSet= PriorityQueue()
-    cameFrom={}
-    grid=[]
-    rods=[]
+    cameFrom={} #to create the path
+    rods=[] #this is used to create local copy of rods and their g and f values
 
+
+    #we compute all possible rods of our labyrithn
     for i in range(numRows):
         for j in range(numCols):
-            rods.append(computeRod((i,j),0,map))
-            rods.append(computeRod((i,j),1,map))
+            rod0=computeRod((i,j),0,map)
+            rod1=computeRod((i,j),1,map)
+            if rod0!=-1 : rods.append(rod0)#, print("generando rod", rod0 )
+            if rod1!=-1 : rods.append(rod1)#, print("generando rod", rod1 )
 
-    #g is the cost from start to current node
-    #print(map)
-    #print(grid)
-    gScore={rod:float('inf') for rod in rods} ##cost to reach current cell
-    gScore[(start)]=0
+    #g is the cost from start to current node max 9999999 movements as for now
+    #f is cost from current cell to the goal f=g + h; max 99999999
+    for r in rods:
+        r.g=999999999#float('inf') pending testing
+        r.f=999999999#float('inf')
+
+    #g cost of initial rod is 0
+    start.g=0
 
     #f=g + h
-    fScore={rod:float('inf') for rod in rods} ##cost from current cell to the goal might be 1000 max
-    fScore[(start)]=h(start,goal)
+    #f cost for initial rod is h
+    start.f=start.h(goal)
 
-    openSet.put((fScore[start],start))
-    #print(openSet.get()[1])
+    #we initialize the openSet with the first node and its fScore
+    openSet.put((start.f,start))
+
     while not openSet.empty():
+        
         current = openSet.get()[1] ## we get the node with lowest fScore
         
-        if current==goal:
+        #check if we reached the goal and stop there
+        if current.cells==goal.cells and current.orientation==goal.orientation:
             return reconstructPath(cameFrom, current)
         
-        neighbors=computeNeighbors(map,current)
-        #print("vecinos de", current, ": ", neighbors)
-        #print("----------\n")
+        neighbors=current.computeNeighbors(map)
         for n in neighbors:
-            #print("explorando vecino", n, "gScore[current]=", gScore[current] )
-            tentative_gScore=gScore[current]+1
-            #print("tentativegScore: ", tentative_gScore, "gScoreN", gScore[n])
-            if tentative_gScore < gScore[n]:
-                cameFrom[n] = current
-                gScore[n] = tentative_gScore
-                fScore[n] = tentative_gScore + h(n,goal)
-                if not any((n in item[1] for item in openSet.queue)):
-                    openSet.put((fScore[n],n))
 
+            #better if we use a dictionary, this is a momentary workaround to keep track of g and f
+            for r in rods:
+                if n.cells==r.cells and n.orientation==r.orientation:
+                    n.g=r.g
+            # tentative_gScore is the distance from start to the neighbor (1 movement)      
+            tentative_gScore=current.g+1
+           
+            if tentative_gScore < n.g:
+                #this path is better than the previosu one, keep it
+                cameFrom[n] = current
+                n.g = tentative_gScore
+                n.f = tentative_gScore + n.h(goal)
+
+                #update g and f for this neighbor in our dictionary
+                for r in rods:
+                    if current.cells==r.cells and n.orientation==current.orientation:
+                        r.g=n.g
+                        r.f=n.f
+                
+                #check if n is in openSet and if not, we add it
+                exists=False
+                for item in openSet.queue:
+                    if item[1].cells==n.cells and item[1].orientation==n.orientation:
+                        exists=True
+                if not exists:
+                    openSet.put((n.f,n))
+
+    #return empty path if there isn't any
     return []
 
+
+
 def computeDestination(map):
-   lastCell=(len(labyrinth)-1,len(labyrinth[0])-1)
-   for i in range(1):
-      if len(computeRod(lastCell,i,map)) != 0:
-        return computeRod(lastCell,i,map)
+    """Computes the destination/final rod, tries to create one per each orientation"""
+    if computeRod((len(labyrinth)-2,len(labyrinth[0])-1),1,map).cells[0] != (0,0):
+        return computeRod((len(labyrinth)-2,len(labyrinth[0])-1),1,map)
+    elif computeRod((len(labyrinth)-1,len(labyrinth[0])-2),0,map).cells[0] != (0,0):
+        return computeRod((len(labyrinth)-1,len(labyrinth[0])-2),0,map)
+    else:   
+        return print("Destination rod couldn't be computed")
+    
     
 
 if __name__=='__main__':
-    #labyrinth=readLabyrinth(sys.argv[1])
+    #read file with labyrinth
+    labyrinth=readLabyrinth(sys.argv[1])
 
-    labyrinth = [[".",".",".",".",".",".",".",".","."],
-                 ["#",".",".",".","#",".",".",".","."],
-                 [".",".",".",".","#",".",".",".","."],
-                 [".","#",".",".",".",".",".","#","."],
-                 [".","#",".",".",".",".",".","#","."]]
+    print("Labyrinth: ")
+    for r in labyrinth:
+        print(r)
+
+    initialRod=Rod(0,[(0,0),(0,1),(0,2)])
+    #print("Initial rod is in cells: ", initialRod.cells, "with orientation ", initialRod.orientation)
     
-    initialRod=[(0,0),(0,1),(0,2)]
     destinationRod=computeDestination(labyrinth)
+    #print("Destination rod is in cells: ", destinationRod.cells, "with orientation ", destinationRod.orientation)
+
+    #print("Starting A* algorithm")
     path=aStar(labyrinth,initialRod,destinationRod)
 
-    print("camino final", path)
+    #print("Final path of rods:")
+    #for r in path:
+        #print(r)
+    if len(path)==0: solution=-1
+    else:
+        solution = len(path)
 
-    solution = len(path)
-
+    print("Result:", solution)
---
+++ code/rod.py
@@ -0,0 +1,88 @@
+class Rod:
+    """class rod which is an 1x3 object in a labyrinth with an orientation, it also has a gScore and fScore"""
+    def __init__(self, orientation=0, cells=[(0,0),(0,1),(0,2)], g=0, f=0):
+        self.cells = cells
+        self.orientation = orientation
+        self.g=g
+        self.f=f
+
+
+    def h(self,rod2):
+        """This function computes the Mahattan distance,is a distance measure that is calculated by taking the sum of distances between the x and y coordinates."""
+        distance=0
+        #in this case as each rod has 3 coordinates, we calculate the distance coordinate per coordinate
+        for i in range(len(self.cells)):
+            distance+=abs(self.cells[i][0]-rod2.cells[i][0]) + abs(self.cells[i][1]-rod2.cells[i][1])
+
+        return distance
+
+    def computeNeighbors(self,map):
+        """Computes all the possible neighbors given a static cell"""
+        numRows=len(map)
+        numCols=len(map[0])
+
+        neighbors=[]
+
+        #there are 5 possible moves for a rod 
+        # R->Rotate 90 degres using center cell as an edge
+        # N-> move up
+        # S-> move down
+        # W-> move left
+        # E-> move right
+        for m in 'RNSWE':
+            newRod=[]
+            valid=True
+            if m=='R':
+                if self.orientation==0:
+                    ###check if we have enough space to rotate 
+                    if self.cells[0][1]+1<numCols and self.cells[0][0]-1>=0 and map[self.cells[0][0]-1][self.cells[0][1]+1]!='#':
+                        if self.cells[2][1]-1<numCols and self.cells[2][0]+1<numRows and map[self.cells[2][0]+1][self.cells[2][1]-1]!='#':
+                            neighbors.append(Rod(1,[(self.cells[0][0]-1,self.cells[0][1]+1),self.cells[1],(self.cells[2][0]+1, self.cells[2][1]-1)],self.g,self.f))               
+                    #check if we have enough space to rotate
+                if self.orientation==1:
+                    if self.cells[0][1]-1<=0 and self.cells[0][0]+1<numRows and map[self.cells[0][0]+1][self.cells[0][1]-1]!='#':
+                        if self.cells[2][1]+1<numCols and self.cells[2][0]-1<numRows and map[self.cells[2][0]-1][self.cells[2][1]+1]!='#':
+                            neighbors.append(Rod(0,[(self.cells[0][0]+1,self.cells[0][1]-1),self.cells[1],(self.cells[2][0]-1,self.cells[2][1]+1)],self.g,self.f))
+        
+            if m=='N':
+                #check if we can move up
+                for c in self.cells:
+                    newRod.append((c[0]-1,c[1]))
+                    if not (c[0]-1>=0 and map[c[0]-1][c[1]]!='#'):
+                        valid=False
+                if valid:
+                    neighbors.append(Rod(self.orientation,newRod,self.g,self.f))
+
+            if m=='S':
+                #check if we can move down
+                for c in self.cells:
+                    newRod.append((c[0]+1,c[1]))
+                    if not (c[0]+1<numRows and map[c[0]+1][c[1]]!='#'):
+                        valid=False
+                if valid:
+                    neighbors.append(Rod(self.orientation,newRod,self.g,self.f))
+
+            if m=='W':
+                #check if we can move left
+                for c in self.cells:
+                    newRod.append((c[0],c[1]-1))
+                    if not (c[1]-1>=0 and map[c[0]][c[1]-1]!='#'):
+                        valid=False
+                if valid:
+                    neighbors.append(Rod(self.orientation,newRod,self.g,self.f))
+
+            if m=='E':
+                #check if we can move right
+                valid=True
+                for c in self.cells:
+                    newRod.append((c[0],c[1]+1))
+                    if not (c[1]+1<numCols and map[c[0]][c[1]+1]!='#'):
+                        valid=False
+                if valid:
+                    neighbors.append(Rod(self.orientation,newRod,self.g,self.f))
+
+        return neighbors
+    
+    def __str__(self):
+        """prints a rod (for debuggin purposes mostly)"""
+        return f"Cells: {self.cells}, orientation: {self.orientation}"
